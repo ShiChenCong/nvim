d56fdf843fdd438a5798463c1d6725ab81953433
ShiChenCong <sccong@outlook.com>
2021-10-21 19:25:30 +0800

    neoscroll config change

--- after/plugin/neoscroll.lua
+++ after/plugin/neoscroll.lua
@@ -17,8 +17,8 @@ local t = {}
 t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '250', [['sine']]}}
 t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '250', [['sine']]}}
 -- Use the "circular" easing function
-t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '220', [['circular']]}}
-t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '220', [['circular']]}}
+t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '250', [['circular']]}}
+t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250', [['circular']]}}
 -- Pass "nil" to disable the easing animation (constant scrolling speed)
 t['<C-y>'] = {'scroll', {'-0.10', 'false', '100', nil}}
 t['<C-e>'] = {'scroll', { '0.10', 'false', '100', nil}}
